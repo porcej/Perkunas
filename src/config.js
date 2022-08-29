0c34f786ec1317d2e9ea08e90dba3da897b5fc2f
Joe Porcelli <porcej@gmail.com>
2022-08-29 06:05:14 -0400

    Updated formating in config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ let env = process.env.NODE_ENV || "development";
 let config = {
   development: require("./config/development.js"),
   production: require("./config/production.js"),
-  staging: require("./config/staging.js")
+  staging: require("./config/staging.js"),
 };
 
 module.exports = config[env];

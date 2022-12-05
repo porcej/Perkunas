51ea50e42bb6860bfae92a1a0459cb6329c93d1e
Joe Porcelli <porcej@gmail.com>
2022-12-05 13:41:03 -0500

    ADDED: Added Swimlanesk display for Staffing data

--- src/config/development.js
+++ src/config/development.js
@@ -1,7 +1,7 @@
 export default {
   cad_url_base: "http://sfireweb2.alexgov.net/CADWebRelay/",
-  dashboard_hub_url: "https://fire-173822vma.alexgov.net:5001/dashboardHub",
-  incident_url: "https://fire-173822vma.alexgov.net:5001/api/Dashboard/incidents",
-  telestaff_url: "https://fire-173822vma.alexgov.net:5001/api/TelestaffProxy/",
-  weather_url: "https://fire-173822vma.alexgov.net:5001/api/WeatherForecast/",
+  dashboard_hub_url: "https://localhost:5001/dashboardHub",
+  incident_url: "https://localhost:5001/api/Dashboard/incidents",
+  telestaff_url: "https://localhost:5001/api/TelestaffProxy/",
+  weather_url: "https://localhost:5001/api/WeatherForecast/",
 };

64433e65bf2a52003046796bfff3275a64e4bec9
Joe Porcelli <porcej@gmail.com>
2023-01-26 09:08:56 -0500

    Added Dashboard links to home page

---
+++ src/components/HelloDashboard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Station Dashboards</h3>
+    <ul>
+      <li>
+        <router-link to="/station/201" target="_blank">201</router-link>
+      </li>
+      <li>
+        <router-link to="/station/202" target="_blank">202</router-link>
+      </li>
+      <li>
+        <router-link to="/station/203" target="_blank">203</router-link>
+      </li>
+      <li>
+        <router-link to="/station/204" target="_blank">204</router-link>
+      </li>
+      <li>
+        <router-link to="/station/205" target="_blank">205</router-link>
+      </li>
+      <li>
+        <router-link to="/station/206" target="_blank">206</router-link>
+      </li>
+      <li>
+        <router-link to="/station/207" target="_blank">207</router-link>
+      </li>
+      <li>
+        <router-link to="/station/208" target="_blank">208</router-link>
+      </li>
+      <li>
+        <router-link to="/station/209" target="_blank">209</router-link>
+      </li>
+      <li>
+        <router-link to="/station/210" target="_blank">210</router-link>
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li v-for="(item, key) in $appConfig" :key="key">
+        <a :href="item" target="_blank" rel="noopener">{{ key }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloDashboard",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+  color: #5c4f7f;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      alt="Vue logo"
+      style="height: 400px; width: auto"
+      src="../assets/afd_logo.jpg"
+    />
+    <HelloDashboard msg="How may I alert you today?" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloDashboard from "@/components/HelloDashboard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    HelloDashboard,
   },
 };
 </script>

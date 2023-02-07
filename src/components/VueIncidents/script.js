53e147e8d2b5cba6e8c2e485481a0e5721f12ec0
Joe Porcelli <porcej@gmail.com>
2023-02-07 10:00:09 -0500

    Add method comments

--- src/components/VueIncidents/script.js
+++ src/components/VueIncidents/script.js
@@ -22,7 +22,7 @@ export default {
     },
     reconnectTicker: {
       type: Number,
-      default: 2000,
+      default: 5000,
     },
     alertForAllIncidents: {
       type: Boolean,
@@ -54,9 +54,7 @@ export default {
   },
 
   mounted() {
-    this.initiHubConnection();
-    // this.$incidentHub.$on('other-added', this.onOtherAdded);
-    // this.$incidentHub.$on('incident-added', this.onIncidentAdded);
+    this.initHubConnection();
     this.$dashboardHub.$on("disconnected", this.onDisconnect);
     this.$dashboardHub.$on("incident-added", this.onIncidentAdded);
     this.$dashboardHub.$on("incident-updated", this.onIncidentUpdated);
@@ -75,38 +73,60 @@ export default {
   },
 
   methods: {
-    initiHubConnection() {
+    /**
+     * Checks if we are connected to a SignalR hub and if we are joins calls
+     * the JoinDashboard hook on the hub then loads units and incidents,
+     * otherwise waits reconnectTicker ms to attempt a reconnect
+     *
+     */
+    initHubConnection() {
       if (this.$dashboardHub.state() === "Connected") {
-        console.log("Connected to hub.");
+        console.info("Connected to hub.");
         this.$dashboardHub.JoinDashboard();
         this.loadIncidents();
         this.loadUnits();
+        this.alertTimer();
       } else {
-        console.log("Connecting...");
+        console.info("Connecting...");
         setTimeout(() => {
-          this.initiHubConnection();
+          this.initHubConnection();
         }, this.reconnectTicker);
       }
     },
+
+    /**
+     * Handles a SignalR disconect event by attempting to reconnect
+     *
+     */
     onDisconnect() {
-      this.initiHubConnection();
+      console.warn("Disconnected from hub.");
+      this.initHubConnection();
     },
+
+    /**
+     * Ticks alert timeout by 1
+     *
+     */
     alertTimer() {
-      if (this.countDown > 0) {
-        setTimeout(() => {
-          Object.keys(this.alertCounters).forEach((cdx) => {
-            if (this.alertCounters[cdx] > this.alertTimeOut) {
-              this.unalertIncident(cdx);
-            } else {
-              this.alertCounters[cdx]++;
-            }
-          });
-          this.alertTimer();
-        }, 1000);
-      }
+      setTimeout(() => {
+        Object.keys(this.alertCounters).forEach((cdx) => {
+          if (this.alertCounters[cdx] > this.alertTimeOut) {
+            this.unalertIncident(cdx);
+          } else {
+            this.alertCounters[cdx]++;
+          }
+        });
+        this.alertTimer();
+      }, 1000);
     },
+
+    /**
+     * Adds an incident to the alerts modal window
+     *
+     * @params {Object} incident Object representing incident to be alerted
+     */
     alertIncident(incident) {
-      console.log(
+      console.info(
         "***************************************\n",
         `**** Alert Requested for Incident ${incident.masterIncidentNumber} `,
         incident,
@@ -118,10 +138,22 @@ export default {
         this.showAlert = true;
       }
     },
+
+    /**
+     * Alerts on the provided incident and starts timeout timer
+     *
+     * @params {Object} incident Object representing incident to be alerted
+     */
     dispatchUnit(incident) {
       this.alertIncident(incident);
       this.alertCounters[incident.id] = 0;
     },
+
+    /**
+     * Removed incident from alerts
+     *
+     * @params {Object} incident Object representing incident to be alerted
+     */
     unalertIncident(incident) {
       const idx = this.alertedIncidents.indexOf(incident);
       if (idx !== -1) {
@@ -129,20 +161,29 @@ export default {
       }
       delete this.alertCounters[incident.id];
     },
+
+    /**
+     * Fetches incidents from API
+     *
+     */
     loadIncidents() {
       return Utils.fetchIncidents({
         url: this.incidentsUrl,
       }).then((data) => {
         this.$set(this, "incidents", data.slice().reverse());
         this.incidents.forEach((idx) => {
-          // this.$dashboardHub.incidentOpened(idx.id);
-          console.log(
+          console.info(
             `Incident ${idx.masterIncidentNumber} with ${idx.id} opened:`,
             idx
           );
         });
       });
     },
+
+    /**
+     * Fetches units from API
+     *
+     */
     loadUnits() {
       return Utils.fetchUnits({
         url: this.unitsUrl,
@@ -153,162 +194,123 @@ export default {
           .map((udx) => udx.radioName);
       });
     },
-    formatTime(timeStr) {
-      let dt = new Date(timeStr);
-      return `${this.padTime(dt.getHours())}:${this.padTime(dt.getMinutes())}`;
-    },
-    padTime(time) {
-      return time.toString().padStart(2, 0);
-    },
-    colorUnit(id) {
-      return `vwi__unit_status_${Utils.getUnitIncidentStatus(id)}`;
-    },
 
     /**
      * Gets called by the incident-added event, adds or updates an incident in
      * the incidents list, then generats an alert if needed
      *
-     * @param {Object} incident new incident information
+     * @param {Object} incident object representing new incident information
      */
     onIncidentAdded(incident) {
-      const thisIncidentIndex = this.incidents.findIndex(
-        (inc) => incident.id === inc.id
-      );
-      if (thisIncidentIndex === -1) {
-        // We don't have a record of this incidnet, lets add it new
-        this.incidents.unshift(incident);
-        console.log(`Incident ${incident.id} opened:`, incident);
-      } else {
-        // We have a record of this incident, lets update it
-        console.log(`Incident ${incident.id} reopened:`, incident);
-        this.incidents[thisIncidentIndex] = incident;
-      }
-      if (this.alertForAllIncidents) {
-        this.dispatchUnit(this.incidents[thisIncidentIndex]);
-      } else {
-        const alertedUnits = incident.unitsAssigned.filter((udx) =>
-          this.unitsToAlert.includes(udx.radioName)
-        );
-        if (alertedUnits.length > 0) {
-          this.dispatchUnit(this.incidents[thisIncidentIndex]);
-          alertedUnits.forEach((udx) =>
-            console.log(`**** Alerted on ${udx} ****`)
-          );
-        }
-      }
+      console.info("\tIncidented Added: ", incident);
     },
+
+    /**
+     * Gets called by the incident-removed event, adds or updates an incident in
+     * the incidents list, then generats an alert if needed
+     *
+     * @param {Number} incidentId unique identifier for the incident to be
+     *                 removed
+     */
     onIncidentRemoved(incidentId) {
-      const idx = this.incidents.findIndex((inc) => incidentId === inc.id);
-      if (idx !== -1) {
-        this.incidents.splice(idx, 1);
-        console.log(`Incident ${incidentId} removed`);
-      }
+      console.info("\tIncident Removed: ", incidentId);
     },
+
+    /**
+     * Gets called by the incidents-removed event, adds or updates an incident
+     * in the incidents list, then generats an alert if needed
+     *
+     * @param {Array} incidentIds array of nunique identifier for the incident
+     *                to beremoved
+     */
     onIncidentsRemoved(incidentIds) {
-      console.log("Removing incidents...");
-      incidentIds.forEach((incidentId) => {
-        this.onIncidentRemoved(incidentId);
-      });
-      console.log("...Incidents removed.");
+      console.info("\tIncidents removed: ", incidentIds);
     },
+
+    /**
+     * Gets called by the incident-removed event, adds or updates an incident
+     * in the incidents list, then generats an alert if needed
+     *
+     * @param {Array} incidentIds array of nunique identifier for the incident
+     *                to beremoved
+     */
     onIncidentUpdated(update) {
-      this.incidents.forEach((incident) => {
-        if (incident.id == update.incidentId) {
-          incident[update.field] = update.value;
-        }
-      });
-    },
-    onIncidentUnitUpdated(update) {
-      console.log("Incident unit update:", update);
-      // Get the incident index
-      const idx = this.incidents.findIndex(
-        (inc) => inc.id === update.incidentId
+      console.info(
+        `\tIncident field change for ${update.indentId} received: `,
+        update
       );
-      if (idx >= 0) {
-        // We have a valid index so we must have the incident
-        // Get the assigned unit's index
-        const udx = this.incidents[idx].unitsAssigned.findIndex(
-          (unit) => unit.radioName === update.unit.radioName
-        );
-        if (udx === -1) {
-          // We don't have the unit... add it and check if we need to alert
-          this.incidents[idx].unitsAssigned.push(update.unit);
-          const adx = this.unitsToAlert.findIndex(
-            (unit) => update.unit.radioName == unit
-          );
-          if (adx >= 0) {
-            this.dispatchUnit(this.incidents[idx]);
-            console.log(`++++ Alerted on ${update.unit.radioName} ++++`);
-          }
-        } else {
-          // Oh good, we have the call and the unit, lets update
-          for (const [key, value] of Object.entries(update.unit)) {
-            this.incidents[idx].unitsAssigned[udx][key] = value;
-          }
-        }
-      }
-      // this.incidents.forEach((incident) => {
-      //   // console.log(`  *** |${typeOf(incident.id)}| - |${typeOf(msg.incidentId)}|`)
-      //   if (incident.id == update.incidentId) {
-      //     if (incident.unitsAssigned == null) {
-      //       incident.unitsAssigned = [update.unit];
-      //     } else {
-      //       let newUnit = true;
-      //       incident.unitsAssigned.forEach((unit) => {
-      //         if (unit.radioName == update.unit.radioName) {
-      //           unit.statusId = update.unit.statusId;
-      //           newUnit = false;
-      //         }
-      //       });
-      //       if (newUnit) {
-      //         incident.unitsAssigned.push({
-      //           radioName: update.unit.radioName,
-      //           statusId: update.unit.statusId,
-      //         });
-      //       }
-      //     }
-      //   }
-      // });
     },
+
+    /**
+     * Gets called by the incident-unit-updated event, adds or updates an unit
+     * attached to a specific incident also updates the unit record
+     *
+     * @param {Object} update Object representing changes to a unit
+     */
+    onIncidentUnitUpdated(update) {
+      console.info(
+        `\tIncident unit update for incident id# ${update.indentId} received: `,
+        update
+      );
+    },
+
+    /**
+     * Gets called by the incident-comment-added event, adds or updates an
+     * incident comment
+     *
+     * @param {Object} comment Object representing a complet or partial
+     *                 incident comment
+     */
     onIncidentCommentAdded(comment) {
-      console.log(
-        `Incident comment added to incident ${comment.incidentId}: `,
+      console.info(
+        `\tIncident comment added to incident ${comment.incidentId}: `,
         comment
       );
-      this.incidents.forEach((incident) => {
-        if (incident.id == comment.incidentId) {
-          if (incident.comments == null) {
-            incident.comments = [];
-          }
-          incident.comments.push(comment.comment);
-        }
-      });
     },
+
+    /**
+     * Gets called by the unit-updated, updates a unit.  if the unit is not
+     * known then a new unit is created that includes the update
+     *
+     * @param {Object} update object representing a change to a unit
+     */
     onUnitUpdated(update) {
-      const udx = this.units.findIndex(
-        (udx) => udx.radioName == update.radioName
+      console.info(
+        `\tUnit: ${update.radioName} updated ${update.field}: ${update.value} `
       );
-      if (udx !== -1) {
-        // Update unit if we found it
-        this.units[udx][update.field] = update.value;
-      } else {
-        // Add data to new unit if we could not find it
-        this.units.push({
-          radioName: update.radioName,
-          [update.field]: update.values,
-        });
-      }
-      // Handle updates to alerting
-      if (update.field === "currentStation") {
-        const sdx = this.unitsToAlert.findIndex(
-          (udx) => udx.radioName === update.radioName
-        );
-        if (update.value === this.station && sdx === -1) {
-          this.unitsToAlert.push(update.radioName);
-        } else if (update.value !== this.station && sdx !== -1) {
-          this.unitsToAlert.splice(sdx, 1);
-        }
-      }
+    },
+
+    /**
+     * Formats timestamp for dashboard display
+     *
+     * @param {String} timeStr String representing date time
+     * @returns {String} Formated string
+     */
+    formatTime(timeStr) {
+      let dt = new Date(timeStr);
+      return `${this.padTime(dt.getHours())}:${this.padTime(dt.getMinutes())}`;
+    },
+
+    /**
+     * Returns time
+     *
+     * @param {Object} time object representing a date and time
+     * @returns {String} Zero padded time string
+     */
+    padTime(time) {
+      return time.toString().padStart(2, 0);
+    },
+
+    /**
+     * Returns a string representing a CSS class corrisponding to the units
+     * status code
+     *
+     * @param {Number} time object representing a date and time
+     * @returns {String} CSS class selector corrisponding to the
+     *                   Unit's Stats code
+     */
+    colorUnit(id) {
+      return `vwi__unit_status_${Utils.getUnitIncidentStatus(id)}`;
     },
   },
 };

9a5cf0ccf84bec264e589cc0471c5440012f798e
Joe Porcelli <porcej@gmail.com>
2023-03-22 13:35:15 -0400

    Updated .eslintrc.js to ignore views

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    'vue/multi-word-component-names': 'off',
+    // 'vue/multi-word-component-names': 'off',
+    "vue/multi-word-component-names": ["error", {
+      "ignores": ["About", "Dash", "Home"]
+    }],
     "prettier/prettier": ["warn", { endOfLine: "auto" }],
   },
   overrides: [
